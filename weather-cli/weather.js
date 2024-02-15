d412d8c243ef492778ca8338a6308063eeeacc1a
Deadmanch <stepanova.nata.r@gmail.com>
2024-02-15 18:34:12 +0300

    исправил опечатку

--- weather-cli/weather.js
+++ weather-cli/weather.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
-import { getArgs } from "./helpers/args.js";
-import { printHelp } from "./services/log.service.js";
+import { getArgs } from './helpers/args.js';
+import { printHelp } from './services/log.service.js';
 const initCLI = () => {
-  const args = getArgs(process.argv);
-  if (args.h) {
-    printHelp();
-  }
-  if (args.s) {
-    // Сохранить город
-  }
-  if (args.t) {
-    // Сохранить токен
-  }
-  // Вывод погоды
+	const args = getArgs(process.argv);
+	if (args.h) {
+		printHelp();
+	}
+	if (args.s) {
+		// Сохранить город
+	}
+	if (args.t) {
+		// Сохранить токен
+	}
+	// Вывод погоды
 };
 
 initCLI();
