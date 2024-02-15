9b14c946fa282101bff3a9cc664a7b8de30c615e
Deadmanch <stepanova.nata.r@gmail.com>
2024-02-15 17:30:46 +0300

    Добавлена стилизация вывода в консоль хелпера, ошибки или успеха

--- weather-cli/weather.js
+++ weather-cli/weather.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
-import { getArgs } from './helpers/args.js';
+
+import { getArgs } from "./helpers/args.js";
+import { printHelp } from "./services/log.service.js";
 const initCLI = () => {
-	const args = getArgs(process.argv);
-	console.log(args);
-	if (args.h) {
-		// Вывод help
-	}
-	if (args.s) {
-		// Сохранить город
-	}
-	if (args.t) {
-		// Сохранить токен
-	}
-	// Вывод погоды
+  const args = getArgs(process.argv);
+  if (args.h) {
+    printHelp();
+  }
+  if (args.s) {
+    // Сохранить город
+  }
+  if (args.t) {
+    // Сохранить токен
+  }
+  // Вывод погоды
 };
 
 initCLI();
