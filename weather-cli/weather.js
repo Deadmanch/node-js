011d4059cc3912c68d327210d989e4613adfcc7d
Deadmanch <stepanova.nata.r@gmail.com>
2024-02-15 22:41:11 +0300

    os и path

--- weather-cli/weather.js
+++ weather-cli/weather.js
@@ -2,6 +2,7 @@
 
 import { getArgs } from "./helpers/args.js";
 import { printHelp } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
 const initCLI = () => {
   const args = getArgs(process.argv);
   if (args.h) {
@@ -11,6 +12,7 @@ const initCLI = () => {
     // Сохранить город
   }
   if (args.t) {
+    saveKeyValue("token", args.t);
     // Сохранить токен
   }
   // Вывод погоды
