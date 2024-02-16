0e141a7b02fefed04f719fcefa0e9c0efee7500d
Deadmanch <stepanova.nata.r@gmail.com>
2024-02-16 21:52:06 +0300

    Получение данных через axios

--- weather-cli/services/api.service.js
+++ weather-cli/services/api.service.js
@@ -1,26 +1,20 @@
-import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
+import { getKeyValue, TOKEN_DICTIONARY } from "./storage.service.js";
+import axios from "axios";
 
-const getWeather = async city => {
-	const token = await getKeyValue(TOKEN_DICTIONARY.token);
-	if (!token) {
-		throw new Error('Не задан ключ API. Задайте его через команду -t [API-KEY]');
-	}
-
-	// const url = new URL('https://api.openweathermap.org/data/2.5/weather');
-	// url.searchParams.append('q', city);
-	// url.searchParams.append('appid', token);
-	// url.searchParams.append('lang', 'ru');
-	// url.searchParams.append('units', 'metric');
-
-	// https.get(url, response => {
-	// 	let res = '';
-	// 	response.on('data', chunk => {
-	// 		res += chunk;
-	// 	});
-	// 	response.on('end', () => {
-	// 		console.log(res);
-	// 	});
-	// });
+const getWeather = async (city) => {
+  const token = await getKeyValue(TOKEN_DICTIONARY.token);
+  if (!token) {
+    throw new Error("Не задан ключ API. Задайте его через команду -t [API-KEY]");
+  }
+  const { data } = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
+    params: {
+      q: city,
+      appid: token,
+      lang: "RU",
+      units: "metric",
+    },
+  });
+  return data;
 };
 
 export { getWeather };
