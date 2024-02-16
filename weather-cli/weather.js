bd05400cbfa5a562394feee2a14f459ef3700974
Deadmanch <stepanova.nata.r@gmail.com>
2024-02-16 11:15:43 +0300

    Добавлена логика работы с файловой системой

--- weather-cli/weather.js
+++ weather-cli/weather.js
@@ -1,21 +1,31 @@
 #!/usr/bin/env node
 
-import { getArgs } from "./helpers/args.js";
-import { printHelp } from "./services/log.service.js";
-import { saveKeyValue } from "./services/storage.service.js";
+import { getArgs } from './helpers/args.js';
+import { printError, printHelp, printSuccess } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async token => {
+	try {
+		await saveKeyValue('token', token);
+		printSuccess('Токен сохранен');
+	} catch (e) {
+		printError(e.message);
+	}
+};
+
 const initCLI = () => {
-  const args = getArgs(process.argv);
-  if (args.h) {
-    printHelp();
-  }
-  if (args.s) {
-    // Сохранить город
-  }
-  if (args.t) {
-    saveKeyValue("token", args.t);
-    // Сохранить токен
-  }
-  // Вывод погоды
+	const args = getArgs(process.argv);
+	if (args.h) {
+		printHelp();
+	}
+	if (args.s) {
+		// Сохранить город
+	}
+	if (args.t) {
+		return saveToken(args.t);
+		// Сохранить токен
+	}
+	// Вывод погоды
 };
 
 initCLI();
